95b3aa73d5ff55f61423289e8cf26d0162484b8b
“enno23” <info@tammena.de>
2017-01-20 16:28:02 +0100

    corrected

--- function_simulationsdatensatz.R
+++ function_simulationsdatensatz.R
@@ -3,28 +3,30 @@ dat <- eusilcA_pop
 dat <- dat[,c(1:15)]
 dat$gender <- as.numeric(dat$gender)-1
 
+#basierend auf dem eusilc wird einkommen geschätzt
 lm1 <- lm(eqIncome ~ ., data = dat)
 
-dat$gender <- as.numeric(dat$gender)-1
-dat$eqIncome <- NULL
- 
 cov_mat <- cov(dat)
 mu <- colMeans(dat)
 
-n <- 80000
-df <- as.data.frame(mvrnorm(n, mu, cov_mat))
+#pop groesse festlegen 
+n_simu <- 80000
+
+df <- as.data.frame(mvrnorm(n_simu, mu, cov_mat))
 df$gender <- df$gender + abs(min(df$gender) )
 df$gender <- df$gender/max(df$gender)
-df$gender <- rbinom(n, 1, df$gender )
+#gender wird wieder in dummy umgewandelt
+df$gender <- rbinom(n_simu, 1, df$gender )
 
-e <- rnorm(n, 0, 10000)
+e <- rnorm(n_simu, 0, 10000)
+#einkommen wird geschätzt, basierend auf obiger regression + fehler
 df$eqIncome <- predict(lm1, df) + e
 df$eqIncome <- ifelse(df$eqIncome>=0, df$eqIncome,0)
 
-df$e <- rnorm(n)
-df$district <-  scale(df$cash) + scale(df$age_ben) + df$e
-df$district <- 
+df$e <- rnorm(n_simu)
 
+#die districte hängen vom cash ab(mit einkommen korreliert)
+df$district <-  scale(df$cash) + scale(df$age_ben) + df$e
 
 df$district <- ((df$district + abs(min(df$district)) ))
 df$district <- df$district *10
