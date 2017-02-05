61cf5cb2575c877b4b1d7acafeb9015b4cd7ca3e
ckoopmann <c.k.e.koopmann@gmail.com>
2017-02-05 12:48:55 +0100

    SimulationData for s = 250, Some additional Plots, enhanced file structure

--- 03_Evaluation.R
+++ 03_Evaluation.R
@@ -1,15 +1,17 @@
 if(!require("data.table")) install.packages("data.table"); library("data.table")
 if(!require("colorRamps")) install.packages("colorRamps"); library("colorRamps")
 if(!require("gridExtra")) install.packages("gridExtra"); library("gridExtra")
+if(!require("ggplot2")) install.packages("ggplot2"); library("ggplot2")
+if(!require("ggthemes")) install.packages("ggthemes"); library("ggthemes")
 
 if(!exists("EvaluationDataPath")){
       EvaluationDataPath <- "EvaluationData"      
 }
 if(!exists("EvaluationDataFilename")){
-      EvaluationDataFilename <- "EvaluationNoSim10Date2017-02-03"   
+      EvaluationDataFilename <- "EvaluationNoSim250Date2017-02-04"   
 }
 if(!exists("SaveMaps")){
-      SaveMaps <- T 
+      SaveMaps <- F
 }
 if(!exists("SaveBoxPlots")){
       SaveBoxPlots <- T
@@ -18,6 +20,11 @@ if(!exists("SaveBoxPlots")){
 
 load(paste(EvaluationDataPath, EvaluationDataFilename, sep = "/"))
 EvaluationDataByRegion <- melt(EvaluationDataByRegion, id.vars = c("Domain","Simulation", "Population")) 
+
+EvaluationDataByRegion[,variable := as.factor(variable)]
+levels(EvaluationDataByRegion$variable) <- c("Weighted", "Unweighted","EBP Unweighted","EBP Weighted", "MSE Estimate", "Sample Size")
+EvaluationDataByRegion[,variable := factor(variable, levels = c("Unweighted","EBP Unweighted","Weighted", "EBP Weighted", "MSE Estimate", "Sample Size"))]
+
 EvaluationByRegion <- EvaluationDataByRegion[,.(MSE = mean((value - Population)^2), MAE = mean(abs(value - Population)), MB = mean(value - Population), MeanEstimate = mean(value), PopulationValue = mean(Population)), by = .(Domain, variable)]
 
 MapsLocation <- paste("Maps",EvaluationDataFilename,"", sep = "/")
@@ -50,17 +57,34 @@ if(SaveMaps){
       }
 }
 
-BoxplotLocation <- "Boxplot"
+
+PlotLocation <- paste("Plots",EvaluationDataFilename,"", sep = "/")
+dir.create(PlotLocation, showWarnings = FALSE)
 if(SaveBoxPlots){
-      MSEVars <- c("gew", "Ungewichtet", "EBP", "EBP_W")
+      MSEVars <- c("Weighted", "Unweighted","EBP Unweighted","EBP Weighted")
       MSEBySimulation <- EvaluationDataByRegion[variable %in% MSEVars,.(MSE = mean((value - Population)^2)), by = .(variable, Simulation)]
       BoxPlotMSEBySimulation <- ggplot(MSEBySimulation, aes(variable, MSE)) + geom_boxplot()
       
       MSEByDomain <- EvaluationDataByRegion[variable %in% MSEVars,.(MSE = mean((value - Population)^2)), by = .(variable, Domain)]
-      BoxPlotMSEByDomain <- ggplot(MSEByDomain, aes(variable, MSE)) + geom_boxplot()
+      BoxPlotMSEByDomain <- ggplot(MSEByDomain, aes(variable, MSE)) + geom_boxplot() +
+            theme_tufte() + geom_rangeframe(sides = "l") + xlab("Estimator") + ylab("Mean Squared Error")
+      ggsave(filename = paste(PlotLocation, "BoxPlotMSEByDomain.png"), device = "png", plot = BoxPlotMSEByDomain, width = 18, height = 12, units = "cm")
       
-      MSEEstimatesByDomain <- EvaluationDataByRegion[variable == "Gini_MSE", .(MSE_Estimate = value),by = .(Domain, Simulation)]
-      MSEEstimatesByDomain <- merge(MSEEstimatesByDomain, MSEByDomain[variable == "EBP"], by = "Domain")
-      BoxPlotMSEEstimate <- ggplot(MSEEstimatesByDomain, aes(variable, MSE_Estimate - MSE)) + geom_boxplot()
+      MAEByDomain <- EvaluationDataByRegion[variable %in% MSEVars,.(MAE = mean(abs(value - Population))), by = .(variable, Domain)]
+      BoxPlotMAEByDomain <- ggplot(MAEByDomain, aes(variable, MAE)) + geom_boxplot() + 
+            theme_tufte() + geom_rangeframe(sides = "l") + xlab("Estimator") + ylab("Mean Absolute Error")
+      ggsave(filename = paste(PlotLocation, "BoxPlotMAEByDomain.png"), device = "png", plot = BoxPlotMAEByDomain, width = 18, height = 12, units = "cm")
+      
+      
+      
+      MSEEstimatesByDomain <- EvaluationDataByRegion[variable == "MSE Estimate", .(MSE_Estimate = value),by = .(Domain, Simulation)]
+      MSEEstimatesByDomain <- merge(MSEEstimatesByDomain, MSEByDomain[variable == "EBP Weighted"], by = "Domain")
+      BoxPlotMSEEstimate <- ggplot(MSEEstimatesByDomain, aes(variable, (MSE_Estimate - MSE))) + geom_boxplot()
+      
+      
+      HistogramMSEEstimate <-  ggplot(MSEEstimatesByDomain, aes((MSE_Estimate - MSE))) + geom_histogram() + 
+            theme_tufte() + geom_rangeframe() + xlab("Error (MSE Estimate - Sample MSE)")
+      ggsave(filename = paste(PlotLocation, "HistogramMSEEstimate.png"), device = "png", plot = HistogramMSEEstimate, width = 18, height = 12, units = "cm")
+      
 }
  
